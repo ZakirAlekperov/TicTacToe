b99e67a642eda97a9c12b77afb906c5b13847770
ZakirAlekperov <zakiralekperov1998@gmail.com>
2022-07-21 21:47:50 +0300

    21321

---
+++ src/TicTacToeEngine.java
@@ -0,0 +1,13 @@
+/**
+ * Игра крестики-нолики на доске 3х3
+ */
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class TicTacToeEngine {
+
+
+
+}
---
+++ src/TicTacToeGUI.java
@@ -0,0 +1,18 @@
+/**
+ * Игра крестики-нолики на доске 3х3
+ */
+
+import javax.swing.*;
+
+public class TicTacToeGUI {
+
+    JButton squares[];
+    JButton newGameButton;
+    JLabel score;
+
+    int emptySquaresLeft = 9;
+
+    public TicTacToeGUI(){
+
+    }
+}
